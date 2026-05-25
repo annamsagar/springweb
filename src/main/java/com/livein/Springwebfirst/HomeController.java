697b3700e0ab52dbba47f9b8ad77e77905258f4c
annamsagar <annamsagar05@gmail.com>
2026-05-25 13:54:26 +0530

    used the prefix and suffix

--- src/main/java/com/livein/Springwebfirst/HomeController.java
+++ src/main/java/com/livein/Springwebfirst/HomeController.java
@@ -6,19 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
     @RequestMapping("add")
     public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
 
         int result=num1+num2;
         model.addAttribute("result",result);
-        return "result.jsp";
+        return "result";
     }
 }
