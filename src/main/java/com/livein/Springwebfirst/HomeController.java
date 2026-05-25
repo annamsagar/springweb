ac11990628bfc009cf1f5198623a0d122740f81c
annamsagar <annamsagar05@gmail.com>
2026-05-25 13:10:16 +0530

    used the model instead of the httpsession

--- src/main/java/com/livein/Springwebfirst/HomeController.java
+++ src/main/java/com/livein/Springwebfirst/HomeController.java
@@ -3,6 +3,7 @@ package com.livein.Springwebfirst;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,11 +15,10 @@ public class HomeController {
         return "index.jsp";
     }
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
 
         int result=num1+num2;
-        session.setAttribute("result",result);
-        System.out.println("result:"+result);
+        model.addAttribute("result",result);
         return "result.jsp";
     }
 }
