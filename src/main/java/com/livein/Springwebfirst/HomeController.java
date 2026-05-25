406a25f58b2543c6a5e7a799e280c3b18e064c0b
annamsagar <annamsagar05@gmail.com>
2026-05-25 12:46:41 +0530

    used the @requestparam to make it simple to navigate the parameters

--- src/main/java/com/livein/Springwebfirst/HomeController.java
+++ src/main/java/com/livein/Springwebfirst/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -13,10 +14,9 @@ public class HomeController {
         return "index.jsp";
     }
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int n1=Integer.parseInt(req.getParameter("num1"));
-        int n2= Integer.parseInt(req.getParameter("num2"));
-        int result=n1+n2;
+    public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, HttpSession session){
+
+        int result=num1+num2;
         session.setAttribute("result",result);
         System.out.println("result:"+result);
         return "result.jsp";
