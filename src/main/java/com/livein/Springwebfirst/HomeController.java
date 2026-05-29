0f9fe759ed964b225d10bf0adb1f88068ddae7f5
annamsagar <annamsagar05@gmail.com>
2026-05-29 17:20:19 +0530

    used the modelandview class to return both obj and name of page

--- src/main/java/com/livein/Springwebfirst/HomeController.java
+++ src/main/java/com/livein/Springwebfirst/HomeController.java
@@ -16,10 +16,11 @@ public class HomeController {
         return "index";
     }
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView model){
 
         int result=num1+num2;
-        model.addAttribute("result",result);
-        return "result";
+        model.addObject("result",result);
+        model.setViewName("result");
+        return model;
     }
 }
